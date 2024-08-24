b7109f3fb6924091e95cf4ee14cac5e1da377313
Lothin Tchappi <lothintchappi@icloud.com>
2024-08-24 09:36:10 +0200

    test

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtins.c                                         :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lnjoh-tc <lnjoh-tc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 23:08:04 by lnjoh-tc          #+#    #+#             */
-/*   Updated: 2024/07/17 13:48:57 by lnjoh-tc         ###   ########.fr       */
+/*   Updated: 2024/08/24 09:29:44 by lnjoh-tc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
-
-Utiliser les DEFINE FAILURE ET SUCCES
-
 int	cd(t_env *envp)
 {
-	char	*path;
+	char	*path = NULL;
 	char	*new_path;
 	char	*last_path;
 
@@ -27,10 +23,10 @@ int	cd(t_env *envp)
 		ft_printf("%s: No such file or directory\n", path);
 		return (FAILURE);
 	}
-	last_path = envp->PATH;
+	last_path = envp->pwd;
 	new_path = get_current_path();
-	envp->PATH = new_path;
-	envp->OLDPWD = last_path;
+	envp->pwd = new_path;
+	envp->oldpwd = last_path;
 	return (SUCCESS);
 }
-*/
+// Lui donnée l'argument 
